cd567debed50730c100d0273e48a23e00401c25b
HongDa Yen <hongda.yen@gmail.com>
2023-06-19 15:02:26 +0800

    refactor: reorder function definitions for readability
    Move the definition of `celery_init_app` above `create_app` to make the call order more clear. This change does not affect the functionality of the code but improves its readability and maintainability.

--- task_app/__init__.py
+++ task_app/__init__.py
@@ -4,6 +4,19 @@ from flask import Flask
 from flask import render_template
 
 
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config['CELERY'])
+    celery_app.set_default()
+    app.extensions['celery'] = celery_app
+    return celery_app
+
+
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_mapping(
@@ -24,16 +37,3 @@ def create_app() -> Flask:
 
     app.register_blueprint(views.bp)
     return app
-
-
-def celery_init_app(app: Flask) -> Celery:
-    class FlaskTask(Task):
-        def __call__(self, *args: object, **kwargs: object) -> object:
-            with app.app_context():
-                return self.run(*args, **kwargs)
-
-    celery_app = Celery(app.name, task_cls=FlaskTask)
-    celery_app.config_from_object(app.config['CELERY'])
-    celery_app.set_default()
-    app.extensions['celery'] = celery_app
-    return celery_app
